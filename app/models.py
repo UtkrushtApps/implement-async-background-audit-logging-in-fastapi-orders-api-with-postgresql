26e9a00972907fed009cdeb62fb18277c92c4bea
Naman Muley <naman.g.muley@gmail.com>
2025-08-02 00:57:43 +0530

    Add app/models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+# Order and OrderAuditLog SQLAlchemy models go here
